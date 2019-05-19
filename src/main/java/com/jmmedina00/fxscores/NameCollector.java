a56bd816f5a3db89c6da68156d5cc95588050bce
jmmedina00 <juanmiguelmedinaprieto@yahoo.es>
2019-05-19 17:04:54 +0200

    Initial commit. Basic structure created and dependencies added (but OpenJFX, which was impossible to add to Maven without relying on non-Maven files)

---
+++ src/main/java/com/jmmedina00/fxscores/NameCollector.java
@@ -0,0 +1,7 @@
+package com.jmmedina00.fxscores;
+
+/**
+ * Class used for collecting random usernames from spinxo.com
+ */
+public class NameCollector {
+}
---
+++ src/main/java/com/jmmedina00/fxscores/struct/Player.java
@@ -0,0 +1,7 @@
+package com.jmmedina00.fxscores.struct;
+
+/**
+ * Class for Players in each team
+ */
+public class Player {
+}
