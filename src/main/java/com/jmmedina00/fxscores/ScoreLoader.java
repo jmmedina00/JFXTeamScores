7c5d3efb44b7d190612694a8056cf5ca43c801fc
jmmedina00 <juanmiguelmedinaprieto@yahoo.es>
2019-05-28 13:42:01 +0200

    Added a status label. File selecting is now working.

--- src/main/java/com/jmmedina00/fxscores/ScoreLoader.java
+++ src/main/java/com/jmmedina00/fxscores/ScoreLoader.java
@@ -3,21 +3,27 @@ package com.jmmedina00.fxscores;
 import com.jmmedina00.fxscores.struct.Player;
 import com.jmmedina00.fxscores.struct.Team;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+
+import java.io.File;
 
 /**
  * Controller class for the main form.
  */
 public class ScoreLoader {
+	private File playerData = null, teamsNames = null;
+
 	@FXML
 	private Button btnShowAll, btnLoadPlayers, btnLoadTeamNames, btnLoadFiles,
 	btnGenerate;
 
 	@FXML
 	private TextField txtPathPlayers, txtPathTeamNames;
+
+	@FXML
+	private Label lblStatus;
 
 	@FXML
 	private TableView<Team> tableTeams;
@@ -31,4 +37,42 @@ public class ScoreLoader {
 
 	@FXML
 	private TableColumn<Player, String> tcPlayerName, tcGlobalPosOrTeam, tcPlayerScore;
+
+	public void loadPlayers() {
+		teamsNames = openDialog(txtPathPlayers, false);
+		boolean disableButtons = (playerData == null || teamsNames == null);
+
+		btnLoadFiles.setDisable(disableButtons);
+		btnGenerate.setDisable(disableButtons);
+	}
+
+	public void loadTeamNames() {
+		playerData = openDialog(txtPathTeamNames, true);
+		boolean disableButtons = (playerData == null || teamsNames == null);
+
+		btnLoadFiles.setDisable(disableButtons);
+		btnGenerate.setDisable(disableButtons);
+	}
+
+	private File openDialog(TextField pathTarget, boolean nonExistent) {
+		File selected = null;
+		FileChooser fileChooser = new FileChooser();
+		Window window = pathTarget.getScene().getWindow();
+
+		if (nonExistent) {
+			fileChooser.setTitle("Open file...");
+			selected = fileChooser.showOpenDialog(window);
+		} else {
+			fileChooser.setTitle("Open or save file...");
+			selected = fileChooser.showSaveDialog(window);
+		}
+
+		if (selected != null) {
+			pathTarget.setText(selected.getAbsolutePath());
+		} else {
+			pathTarget.setText("");
+		}
+
+		return selected;
+	}
 }
